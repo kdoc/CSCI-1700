5ad40c1ccb4144cc647de2c600b0a512661f75d0
kdoc <31784819+kdoc@users.noreply.github.com>
2017-09-11 02:12:54 -0500

    Update Problem5.java

--- Problem5.java
+++ Problem5.java
@@ -1,13 +1,15 @@
 package assignment1;
-
+/**
+ Problem 5
+ @authors Keith Docka, Isse Nur, Xavier Jackson
+ CSCI 1700-01
+ */
 public class GearCadenceSpeed 
 {
-	public static final double PI = 3.14159265358979323846;
-	
 	public static void main(String[] args)
 	{
 		double gearSize = 100.0, cadence = 90.0, speed = 0;
-		speed = gearSize * PI * (60 / 63360.0) * cadence;
+		speed = gearSize * Math.PI * (60 / 63360.0) * cadence;
 		System.out.println("With a gear size of " + gearSize + " and a cadence of " + cadence 
 				           + " you will bike "+ speed + " miles per hour.");
 	}
