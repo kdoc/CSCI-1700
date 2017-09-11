5d8a71765bbe1ad58abd22dcbc124e4f7d61cc05
kdoc <31784819+kdoc@users.noreply.github.com>
2017-09-11 02:15:08 -0500

    Update Problem8.java

--- Problem8.java
+++ Problem8.java
@@ -1,5 +1,9 @@
 package assignment1;
-
+/**
+Problem 8
+@authors Keith Docka, Isse Nur, Xavier Jackson
+CSCI 1700-01
+*/
 import java.util.Scanner;
 
 public class VendingMachine 
@@ -9,15 +13,16 @@ public class VendingMachine
 		int item = 0, change = 0, quarter =0, dime = 0, nickel = 0;
 		Scanner keyBoard = new Scanner(System.in);
 		System.out.println("Enter price of item");
-		System.out.println("(from 25 cents to a dollar, in 5-cent increments)");
+		System.out.println("(from 25 cents to a dollar, in 5-cent increments):");
 		item = keyBoard.nextInt();
 		change = (100 - item);
 		quarter = (change / 25);
 		dime = ((change % 25) / 10);
 		nickel = (((change % 25) % 10) / 5);
 		System.out.printf("You bought an item for %d cents and gave me a dollar,%n", item);
-		System.out.printf("So your change is%n%-2dquarters,%n%-2ddimes, and%n%-2dnickels.", 
+		System.out.printf("So your change is%n%4d quarters,%n%4d dimes, and%n%4d nickels.", 
 				          quarter, dime, nickel);
+		keyBoard.close();
 	}
 
 }
