5d5f09108f8dbb2f4ba0c5e9d4532482f3e093be
kdoc <31784819+kdoc@users.noreply.github.com>
2017-11-30 02:19:31 -0600

    Add files via upload

---
+++ Problem6.java
@@ -0,0 +1,50 @@
+package assignment5;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class Problem6 
+{
+	public static String searchForFile(File dir, String target)
+	{
+		String result = "";
+		if(!dir.isDirectory())
+			return "Path is not a directory.";
+		for(File folderItem : dir.listFiles())
+		{
+			if(folderItem.isDirectory())
+			{
+				result = searchForFile(folderItem, target);
+			}
+			else
+			{
+				if(folderItem.getName().equals(target))
+					System.out.println("The file: "
+							+ target + " has been found "
+							+ "in the following folder:\n"
+							+ folderItem.getAbsolutePath());
+			}
+		}
+		return "";
+	}
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String prompt = "";
+		do
+		{
+			System.out.println("Enter the directory / folder to search: ");
+			File rootFolder = new File(keyboard.nextLine());
+			System.out.println("Enter the file to search: ");
+			String fileToFind = keyboard.nextLine();
+			searchForFile(rootFolder, fileToFind);
+			System.out.println("End of search.\r\n" + 
+					"\r\n" + 
+					"Another search? Type y to continue, n to exit:\r\n");
+			prompt = keyboard.nextLine();
+		}while(prompt.equalsIgnoreCase("y"));
+		System.out.println("Process completed.");
+		keyboard.close();
+	}
+}
